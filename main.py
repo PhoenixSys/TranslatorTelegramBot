00b83963032ea9a186732a24222865b9c0e54712
Mobin <Mobinatashi2@gmail.com>
2022-03-05 20:10:54 +0330

    start working image translator

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from mongodb_manager import DataBaseManagerUser
 import telebot
+from PIL import Image
+import pytesseract
+from googletrans import Translator
 from telebot import types
 
 TOKEN = "5272226190:AAHvQaAECllfCEFQqpodfspZo1GCi5dw8YE"
@@ -14,7 +17,7 @@ def service1_command(message):
 
 
 @bot.message_handler(commands=["register"])
-def service1_command(message):
+def service2_command(message):
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     button_phone = types.KeyboardButton(text="Send phone",
                                         request_contact=True)
@@ -39,6 +42,22 @@ def check_login(message):
         bot.send_message(message.chat.id, 'You Are Registered')
     else:
         bot.send_message(message.chat.id, 'Please Use /register for login in this bot')
+
+
+@bot.message_handler(content_types=['photo'])
+def contact(message):
+    fileID = message.photo[-1].file_id
+    file_info = bot.get_file(fileID)
+    downloaded_file = bot.download_file(file_info.file_path)
+    with open(f"{fileID}.jpg", 'wb') as new_file:
+        new_file.write(downloaded_file)
+    img = Image.open(f'{fileID}.jpg')
+    result = pytesseract.image_to_string(img)
+    print(result)
+    p = Translator()
+    p_translated = p.translate(result, dest='french')
+    translated = str(p_translated.text)
+    bot.send_message(message.chat.id, 'Your Photo Received !')
 
 
 bot.polling()
